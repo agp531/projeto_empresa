2081e3bdcd2dfc2993d6ab30649d87f8b59d1abd
Rafael da Rosa <101129583+Rafaelh531@users.noreply.github.com>
2022-10-11 11:07:00 -0300

    Create pag_principal.js

---
+++ Front-end/principal/pag_principal.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: "#app",
+    vuetify: new Vuetify(),
+
+    data: {
+        drawer: false,
+        group: null,
+    }, // Fim data:
+
+    computed: {
+
+    }, // Fim computed:
+
+    methods: {
+
+    }, // Fim methods:
+});
